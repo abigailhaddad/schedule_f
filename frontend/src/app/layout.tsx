7e26ecfa5cf04a5abb1a2d80bcaf8d3474d97e3c
Michael Boyce <michael.e.boyce@gmail.com>
2025-05-20 12:47:44 -0400

    add a little logo

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,12 +2,23 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { datasetConfig } from '@/lib/config'
+import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: datasetConfig.title,
   description: 'Regulatory Comments Analysis',
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: '32x32' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+    ],
+    apple: {
+      url: '/apple-touch-icon.png',
+      sizes: '180x180',
+    },
+  },
 }
 
 export default function RootLayout({
